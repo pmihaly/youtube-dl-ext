4194fb950cfb339c9135383cd7143c51d75667a3
pmihaly <mishreg@protonmail.com>
2020-08-20 10:49:33 +0200

    szerver integráció teszt (lemezre írás nélkül)

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,10 +1,32 @@
+const fs = require('fs');
+const path = require('path');
 const request = require('supertest');
+const dotenv = require('dotenv').config();
 const app = require('../server');
 
 describe('GET /ping', () => {
-  it('responds with json', (done) =>
-    request(app).get('/ping').expect('Content-Type', /json/).expect(200, done()));
+  const req = request(app).get('/ping');
 
-  it("responds with 'pong'", (done) =>
-    request(app).get('/ping').expect(200, { result: 'pong' }, done()));
+  it('responds with a json', (done) => req.expect('Content-Type', /json/).expect(200, done()));
+
+  it("responds with 'pong'", (done) => req.expect(200, { result: 'pong' }, done()));
+});
+
+describe('GET /:videoId', () => {
+  const req = request(app).get('/CH0ix7D-vr0');
+
+  it('responds correctly', (done) =>
+    req.expect('Content-Type', /json/).expect(
+      200,
+      {
+        success: true,
+        videoPath: `${path.join(
+          process.env.DL_PATH,
+          'City Morgue - Toxic Boogaloo EP REVIEW'
+        )}.mkv`,
+      },
+      done()
+    ));
+
+    // TODO lemerze írás tesztje
 });
