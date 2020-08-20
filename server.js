7cff86f1a57d75bb565f072e21fd5a6d728c9336
pmihaly <mishreg@protonmail.com>
2020-08-20 09:55:55 +0200

    ping-pong útvonal

--- server.js
+++ server.js
@@ -10,14 +10,16 @@ app.use(morgan('tiny'));
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/ping', (_, res) => {
+  res.json({ result: 'pong' });
+});
+
 app.get('/:videoId', (req, res) => {
   const { videoId } = req.params;
 
   ytdl.getInfo(videoId).then((info) => {
     const { title } = info.videoDetails;
     const videoPath = `${path.join(process.env.DL_PATH, title)}.mkv`;
-
-    console.log(`Downloading "${title}"`);
 
     ytdl(`https://youtube.com/watch?v=${videoId}`).pipe(fs.createWriteStream(videoPath));
 
